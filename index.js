38e19e56495599f09dc9c5413747be4131b5adb6
Anthony Phillips <anthony.s.phillips888@gmail.com>
2024-06-05 15:49:53 -0400

    Create setter methods

--- index.js
+++ index.js
@@ -15,4 +15,15 @@ class Circle {
     return (Math.PI) * this.radius ** 2
   }
 
+  set diameter(diameter) {
+    this.radius = diameter /2
+  }
+
+  set circumference(circumference) {
+    this.radius = circumference / (Math.PI * 2)
+  }
+
+  set area(area) {
+    this.radius = Math.sqrt(area/(Math.PI))
+  }
 }
